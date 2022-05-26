3c253886de2b744ea85119c798d97c6b65510931
JuKu <kuenzel.justin@t-online.de>
2022-05-26 17:09:53 +0200

    Change pubspec.yaml, add dependency for dartz, add IdeaUseCases

---
+++ lib/domain/usecases/idea_usecases.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+
+class IdeaUseCases {
+  IdeaUseCases() {
+    //
+  }
+
+  Future<bool> addIdea(String title, String description) async {
+    return false;
+  }
+}
