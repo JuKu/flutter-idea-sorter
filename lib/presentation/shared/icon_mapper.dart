b8df69f7d32f720e9ea5444a8ace3fddb23251f1
JuKu <kuenzel.justin@t-online.de>
2022-06-09 17:49:34 +0200

    [#182325738] Add support to show the idea icon in idea overview

--- lib/presentation/shared/icon_mapper.dart
+++ lib/presentation/shared/icon_mapper.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 
 class IconMapper {
+  static final Map<String, IconData> _iconList = {
+    Icons.add.toString(): Icons.add,
+    Icons.question_mark.toString(): Icons.question_mark
+  };
+
   /// private constructor
   IconMapper._();
 
   static IconData getIcon(String iconName) {
     /// TODO: add other icons
+
+    if (_iconList.containsKey(iconName)) {
+      return _iconList[iconName]!;
+    }
 
     /// if the icon is unknown
     return Icons.question_mark;
